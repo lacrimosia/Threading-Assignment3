ed45984ce368b6c47fc18d77b1cbaef24e22cfbf
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-05-19 21:24:10 -0700

    added files

---
+++ mathSolver.cpp
@@ -0,0 +1,48 @@
+/*-------------------------------------------------------------------------*
+ *---                 ---*
+ *---   mathSolver.cpp            ---*
+ *---                 ---*
+ *---     This file defines the high-level functions of the math  ---*
+ *--- generator and solver program.         ---*
+ *---                 ---*
+ *--- ----  ----  ----  ----  ----  ----  ----  ----  ---*
+ *---                 ---*
+ *--- Version 1a    2019 May 6    Joseph Phillips ---*
+ *---                 ---*
+ *-------------------------------------------------------------------------*/
+
+//
+//  Compile with:
+//  $ g++ mathSolver.cpp -o mathSolver -lpthread -g
+//
+
+#include "mathSolverHeader.h"
+#include "pthread.h"
+
+void* evaluate(void* vPtr)
+{
+    NodeBuffer* nodeBufferPtr = (NodeBuffer*)vPtr;
+
+    //  YOUR CODE HERE
+}
+
+//  PURPOSE:  To return a randomly generated Node.
+Node* makeNode()
+{
+    return ((rand() % 3) ? (Node*)new ConstNode() : (Node*)new OperatorNode());
+}
+
+int main(int argc,
+    char* argv[])
+{
+    NodeBuffer nodeBuffer;
+    pthread_t consumer0;
+    pthread_t consumer1;
+    int toReturn = EXIT_SUCCESS;
+
+    srand((argc < 2) ? getpid() : atoi(argv[1]));
+
+    //  YOUR CODE HERE
+
+    return (toReturn);
+}
