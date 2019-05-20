f7c6380c1133f6e32bd50091762aaec53f31dc59
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-05-20 00:18:53 -0700

    Added more code

--- mathSolver.cpp
+++ mathSolver.cpp
@@ -21,11 +21,10 @@
 void* evaluate(void* vPtr)
 {
     NodeBuffer* nodeBufferPtr = (NodeBuffer*)vPtr;
-    Node* nodePtr;
 
     //  YOUR CODE HERE
-    while(NUM_PROBLEMS/2){
-        *nodePtr = *nodeBufferPtr->pullOut();
+    for(int i = 0;  i < NUM_PROBLEMS / 2;  i++){
+        nodeBufferPtr->pullOut();
 
         // print value
         printf("number %d \n",nodePtr->toString().c_str());
@@ -49,12 +48,14 @@ int main(int argc,
     NodeBuffer nodeBuffer;
     pthread_t consumer0;
     pthread_t consumer1;
+    pthread_mutex_t lock;
     int toReturn = EXIT_SUCCESS;
 
     srand((argc < 2) ? getpid() : atoi(argv[1]));
 
     //  YOUR CODE HERE
     // Created child threads
+    pthread_mutex_init(&lock,NULL);
     pthread_create(&consumer0,NULL,evaluate,(void*)&nodeBuffer);
     pthread_create(&consumer1,NULL,evaluate,(void*)&nodeBuffer);
 
@@ -63,7 +64,7 @@ int main(int argc,
         //make the nodes up until NUM_PROBLEMS
         makeNode();
         // Put the return address into node buffers
-        nodeBuffer = (Node*)&makeNode();
+        nodeBuffer->putIn(&makeNode());
     }
 
     // wait until they finish
@@ -72,6 +73,8 @@ int main(int argc,
 
     void *intPtr2;
     pthread_join(&consumer1, (void **)&intPtr2);
+
+    pthread_mutex_destroy(&lock);
 
     return (toReturn);
 }
